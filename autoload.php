7fe7d32b23ca8d6a1ac97444627238f30778dccf
ahmadreza1383 <ahmadrezabashari@gmail.com>
2023-03-04 20:29:35 +0330

    Lower the first letter of the namespace~

--- autoload.php
+++ autoload.php
@@ -1,5 +1,7 @@
 <?php
+
 spl_autoload_register(function ($class_name) {
+    $class_name[0] = strtolower($class_name[0]);
     $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
     include $class_name . '.php';
 });
